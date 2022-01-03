563a082b910a33f72ca51e037c46d5422d4420da
EpiCanard <epicanard@gmail.com>
2022-01-04 00:02:04 +0100

    [nvim] Add mapping to delete buffer with telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,6 +2,11 @@ require('telescope').setup{
   defaults = {
     path_display = {
       shorten = { len = 1, exclude = {1, -2, -1} }
+    },
+    mappings = {
+      n = {
+        ['dd'] = require('telescope.actions').delete_buffer
+      },
     }
   }
 }
