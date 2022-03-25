27346c722823155450bbb40fd15f40fab0c5c798
EpiCanard <epicanard@gmail.com>
2022-03-25 08:52:33 +0100

    [keymap] Add BTN 4-5 and set wheel config

--- config.h
+++ config.h
@@ -5,3 +5,11 @@
 
 #define TRACKBALL_OFF_TIMEOUT 650 // ms
 #define TRACKBALL_SMOOTHING_CYCLES 10
+
+// Remove ergodox_ez MOUSEKEY config to allow to override
+#define MOUSEKEY_INTERVAL          80
+#define MOUSEKEY_MAX_SPEED         4
+#define MOUSEKEY_WHEEL_DELAY       0
+#define MOUSEKEY_TIME_TO_MAX       80
+
+#define MOUSEKEY_MOVE_DELTA        8
