a0441854dc2a1c9991026b037c77597e719f8476
EpiCanard <epicanard@gmail.com>
2022-05-23 23:09:03 +0200

    [nvim] Add tap dance for shift/caps #6

--- keymap/tap_dance.c
+++ keymap/tap_dance.c
@@ -2,5 +2,6 @@
 #include "tap_dance.h"
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_CTRL_CMD] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_LGUI)
+    [TD_CTRL_CMD] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_LGUI),
+    [TD_SHIFT_HOLD] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
 };
--- keymap/tap_dance.h
+++ keymap/tap_dance.h
@@ -1,5 +1,7 @@
 #define TD_LCTL TD(TD_CTRL_CMD)
+#define TD_LSFT TD(TD_SHIFT_HOLD)
 
 enum tap_dance_keys {
-    TD_CTRL_CMD
+    TD_CTRL_CMD,
+    TD_SHIFT_HOLD
 };
