eb7cccefa665660710edeffa5f85c2286b704170
EpiCanard <epicanard@gmail.com>
2022-07-18 14:22:46 +0200

    [nvim] Add scala autopairs for """

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -1,1 +1,11 @@
-require('nvim-autopairs').setup{}
+local autopairs = require('nvim-autopairs')
+local Rule = require('nvim-autopairs.rule')
+
+autopairs.setup{
+  fast_wrap = {}
+}
+
+autopairs.add_rules({
+  Rule('"""', '"""', 'scala')
+    :replace_map_cr(function() return '\n  |\n|<c-o>$.stripMargin<Up>' end),
+})
