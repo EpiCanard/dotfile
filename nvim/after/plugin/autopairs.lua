217e9560e6dea6541f4fdc94206c8a2c6f8a434c
EpiCanard <epicanard@gmail.com>
2022-07-21 00:08:39 +0200

    [nvim] Improve """ autopairs to add | on CR

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -1,5 +1,6 @@
 local autopairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
 
 autopairs.setup{
   fast_wrap = {}
@@ -8,4 +9,8 @@ autopairs.setup{
 autopairs.add_rules({
   Rule('"""', '"""', 'scala')
     :replace_map_cr(function() return '\n  |\n|<c-o>$.stripMargin<Up>' end),
+  Rule('^%s+|.*', '', 'scala')
+    :use_regex(true)
+    :with_pair(cond.none())
+    :replace_map_cr(function() return '\n|' end),
 })
