680ccd707b9e94958f787554fc7d2493f1cd47b5
EpiCanard <epicanard@gmail.com>
2023-03-16 14:57:25 +0100

    [nvim] Add auto string context on $ and docs for autopairs

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -7,12 +7,26 @@ autopairs.setup({
 })
 
 autopairs.add_rules({
+  -- Generate stripMargin multine text
+  -- Before   ~  Input  ~  After
+  -- """|"""  ~  <CR>   ~  """
+  --          ~         ~    ||
+  --          ~         ~    |""".stripMargin
   Rule('"""', '"""', "scala"):replace_map_cr(function()
     return "\n  |\n|<c-o>$.stripMargin<Up>"
   end),
+  -- Auto add | on multiline string context
+  -- Before              ~  Input  ~  After
+  -- """                 ~  <CR>   ~  """
+  --   | text|           ~         ~    | text
+  --   |""".stripMargin  ~         ~    ||
+  --                     ~         ~    |""".stripMargin
   Rule("^%s+|.*", "", "scala"):use_regex(true):only_cr():replace_map_cr(function()
     return "\n|"
   end),
+  -- Add {} arround parameter in string context when . is pressed
+  --  Before    ~  Input  ~     After
+  -- "$myvar|"  ~    .    ~  "${myvar.|}"
   Rule("%$(%a+)%.$", "", "scala"):use_regex(true):replace_endpair(function(opts)
     vim.api.nvim_feedkeys(
       vim.api.nvim_replace_termcodes("<esc>F$a{<esc>" .. opts.col + 2 .. "|i}", true, false, true),
@@ -21,6 +35,20 @@ autopairs.add_rules({
     )
     return ""
   end),
+  -- Add s in front of "" when there is a $ in text.
+  -- Before  ~  Input  ~  After
+  --  "|"    ~    $    ~  s"$|"
+  Rule('%"([%a%s]*)%$$', "", "scala"):use_regex(true):replace_endpair(function(opts)
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes('<esc>F"is<esc>' .. opts.col + 2 .. "|a", true, false, true),
+      "n",
+      false
+    )
+    return ""
+  end),
+  -- Add {} arround import when , is pressed
+  -- Before                      ~  Input  ~  After
+  -- import package.FirstClass|  ~    ,    ~  import package.{FirstClass, |}
   Rule("^import ([%w%._]+),$", "", "scala"):use_regex(true):replace_endpair(function(opts)
     vim.api.nvim_feedkeys(
       vim.api.nvim_replace_termcodes("<esc>F.a{<esc>" .. opts.col + 1 .. "|a }", true, false, true),
