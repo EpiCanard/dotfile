f0e904e139f41cd1c20cf6dadf5c7c807699c149
EpiCanard <epicanard@gmail.com>
2023-08-31 14:13:23 +0200

    [nvim] Fix les erreurs avec nap et git signs

--- nvim/after/plugin/nap.lua
+++ nvim/after/plugin/nap.lua
@@ -1,7 +1,43 @@
-require("nap").setup({
+local nap = require("nap")
+
+nap.setup({
   next_prefix = "<leader><CR>",
   next_repeat = "<c-j>",
   prev_repeat = "<c-k>",
 })
 
-require("nap").nap("c", "Gitsigns next_hunk", "Gitsigns prev_hunk", "Next diff", "Previous diff")
+local function gitsigns()
+  -- Whether we are in a diff mode where "]c" "[c" will likely work better for hunks.
+  local function in_diff_mode()
+    return vim.wo.diff or vim.bo.filetype == "fugitive" or vim.bo.filetype == "git"
+  end
+  return {
+    next = {
+      rhs = function()
+        if in_diff_mode() then
+          return "]c"
+        end
+        vim.schedule(function()
+          require("gitsigns").next_hunk({ preview = false })
+        end)
+        return "<Ignore>"
+      end,
+      opts = { desc = "Next diff", expr = true },
+    },
+    prev = {
+      rhs = function()
+        if in_diff_mode() then
+          return "[c"
+        end
+        vim.schedule(function()
+          require("gitsigns").prev_hunk({ preview = false })
+        end)
+        return "<Ignore>"
+      end,
+      opts = { desc = "Prev diff", expr = true },
+    },
+    mode = { "n", "x", "o" },
+  }
+end
+
+nap.map("c", gitsigns())
